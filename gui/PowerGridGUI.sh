9b7c37991e4c048df1a54c9ac37bb190eb38f0ea
SariItani <74187768+SariItani@users.noreply.github.com>
2024-06-01 01:13:31 +0300

    hmm

--- gui/PowerGridGUI.sh
+++ gui/PowerGridGUI.sh
@@ -3,6 +3,7 @@
 
 # Set the path to your JAR file
 JAR_PATH="/home/sari-itani/Documents/GitHub/EV-SOL/gui/PowerGridGUI.jar"
+JSON_LIB_PATH="/home/sari-itani/Documents/GitHub/EV-SOL/libs/json-20230618.jar"
 
 # Execute the Java application
-java -jar $JAR_PATH
+java -cp $JAR_PATH:$JSON_LIB_PATH PowerGridGUI
